2896f64fe5728af66754a500c002d8e273a1883c
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-13 15:11:57 -0700

    initial commit

---
+++ src/components/Styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CenterDiv = styled.div`
+    text-align: center;
+    margin: auto;
+    padding: 20px;
+`;
---
+++ src/pages/SinglePlayer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SinglePlayer = () => {
+
+    return (
+        <div>
+            <p>1 player</p>
+        </div>
+    );
+};
+
+export default SinglePlayer;
