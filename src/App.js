14ffe827d072e4ce2a903391841fee2a55d50302
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-14 10:24:26 -0700

    added browerRouter and home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
+import { Route, Routes } from 'react-router';
+import { Link } from 'react-router-dom';
 import './App.css';
 import { CenterDiv } from './components/Styles';
+import Home from './pages/Home';
 import SinglePlayer from './pages/SinglePlayer';
 
 function App() {
   return (
     <CenterDiv>
-      <h1>Rock, Paper Scissors!</h1>
-      <SinglePlayer />
+      <h1>Rock, Paper, Scissors!</h1>
+      <Link to="/">Home</Link>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/single" element={<SinglePlayer/>} />
+      </Routes>
     </CenterDiv>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Segment, Button } from "semantic-ui-react";
+
+const Home = () => {
+
+    const navigate = useNavigate();
+
+    return (
+        <Segment>
+            <h1>Play Single Player</h1>
+            <Button onClick={()=>navigate("/single")} >Play</Button>
+            <h1>Play Two Player</h1>
+            <Button></Button>
+        </Segment>
+    );
+};
+
+export default Home;
