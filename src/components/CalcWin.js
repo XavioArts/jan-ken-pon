d5f6c8392ed255dae0920097f2e20ae807ebd89e
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-17 12:40:27 -0700

    warning fix

--- src/components/CalcWin.js
+++ src/components/CalcWin.js
@@ -4,51 +4,51 @@ import { Dimmer, Loader, Segment } from "semantic-ui-react";
 const CalcWin = (props) => {
     const {selection, compChoice, player, computer, setWinState, setCalculating, setResultColor, dispatch} = props;
 
-    const calculateWin = () => {
-        let winner = "";
-        let result = null;
-        // console.log(selection);
-        // console.log(compChoice);
-        if (selection === compChoice) {
-            result = "tie";
-        } else {
-            // handle wins and losses
-            if (selection === "rock" && compChoice === "scissors") {
-                result = "win";
-                winner = player.name;
-                player.wins++;
-            } else if (selection === "paper" && compChoice === "rock") {
-                result = "win";
-                winner = player.name;
-                player.wins++;
-            } else if (selection === "scissors" && compChoice === "paper") {
-                result = "win";
-                winner = player.name;
-                player.wins++;
-            } else {
-                result = "loss";
-                winner = computer.name;
-                computer.wins++;
-            }
-        }
-
-        if (result === "tie") {
-            // return <h1>It's a tie!</h1>
-            setWinState("It's a tie!");
-            setResultColor("black");
-        } else {
-            setWinState(`${winner} wins!`);
-            if (result === "win") {
-                setResultColor("green");
-            } else {
-                setResultColor("red");
-            }
-        }
-        setCalculating(false);
-        dispatch({type: result});
-    };
-
+    
     useEffect(()=>{
+        const calculateWin = () => {
+            let winner = "";
+            let result = null;
+            // console.log(selection);
+            // console.log(compChoice);
+            if (selection === compChoice) {
+                result = "tie";
+            } else {
+                // handle wins and losses
+                if (selection === "rock" && compChoice === "scissors") {
+                    result = "win";
+                    winner = player.name;
+                    player.wins++;
+                } else if (selection === "paper" && compChoice === "rock") {
+                    result = "win";
+                    winner = player.name;
+                    player.wins++;
+                } else if (selection === "scissors" && compChoice === "paper") {
+                    result = "win";
+                    winner = player.name;
+                    player.wins++;
+                } else {
+                    result = "loss";
+                    winner = computer.name;
+                    computer.wins++;
+                }
+            }
+    
+            if (result === "tie") {
+                // return <h1>It's a tie!</h1>
+                setWinState("It's a tie!");
+                setResultColor("black");
+            } else {
+                setWinState(`${winner} wins!`);
+                if (result === "win") {
+                    setResultColor("green");
+                } else {
+                    setResultColor("red");
+                }
+            }
+            setCalculating(false);
+            dispatch({type: result});
+        };
         calculateWin();
     }, []);
 
